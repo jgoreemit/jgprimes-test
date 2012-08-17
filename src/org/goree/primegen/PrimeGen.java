d6a60b73a4a781b5eb0e5d6dacb1888154c1ee94
John Goree <john.goree@galtsgulch>
2012-08-17 16:16:31 -0700

    Initial version of prime generator

---
+++ src/org/goree/primegen/PrimeGen.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.goree.primegen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author JG
+ */
+public class PrimeGen {
+    
+    
+    private static void addNextPrime(List<Integer> primes) {
+        if (primes.isEmpty()) {
+            primes.add(2);
+            return;
+        } 
+        int nextCandidate = primes.get(primes.size() - 1) + 1;
+        while (true) {
+            if (isPrime(nextCandidate, primes)) {
+                primes.add(nextCandidate);
+                return;
+            } else {
+                nextCandidate++;
+            }
+        }
+    }
+    
+    
+    public static boolean isPrime(int x, List<Integer> primes) {
+        for (int prime : primes) {
+            if (x % prime == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    public static void main(String[] args) throws Exception {
+        List<Integer> primes = new ArrayList<Integer>();
+        while (primes.size() < 100) {
+            addNextPrime(primes);
+        }
+        System.out.println(primes);
+    }
+
+}
